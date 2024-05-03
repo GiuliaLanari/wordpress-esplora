374b1f2b6dd66afa3890c623eb5a34dfdcdb51f9
GiuliaLanari <giulia.lanari96@gmail.com>
2024-05-03 15:28:29 +0200

    video home-page

--- wp-content/themes/special-team3/front-page.php
+++ wp-content/themes/special-team3/front-page.php
@@ -2,11 +2,19 @@
 
 get_header(); 
 ?>
+<div class="container-fluid">
+<div class="row">
+
 
 <h1><?php the_title(); ?></h1>
-<p><?php the_archive_description(); ?></p>
 
-
-
+<div class="col-12">
+<video  loop muted>
+  <source src="./wp-content/themes/special-team3/assets/img/4205790-uhd_2880_2160_30fps.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+</div>
+</div> 
+</div>
 <?php
 get_footer();
