86a9fbe0b09bb6a8ab42a50f51edd9e384c6cb14
GiuliaLanari <giulia.lanari96@gmail.com>
2024-05-03 17:21:31 +0200

    add video and logo

--- wp-content/themes/special-team3/front-page.php
+++ wp-content/themes/special-team3/front-page.php
@@ -5,14 +5,16 @@ get_header();
 <div class="container-fluid">
 <div class="row">
 
+<div class="col-12 px-0 overflow-hidden position-relative " style="height: 100dvh;">
 
-<h1><?php the_title(); ?></h1>
+  <img src="./wp-content/themes/special-team3/assets/img/epic-road.png" alt="logo" class="position-absolute mt-2 ms-2 video-container" width="20%"/>
 
-<div class="col-12">
-<video  loop muted>
-  <source src="./wp-content/themes/special-team3/assets/img/4205790-uhd_2880_2160_30fps.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
+    <video class="object-fit-cover w-100" autoplay loop muted >
+       <source src="./wp-content/themes/special-team3/assets/img/Egypt.mp4" type="video/mp4">
+         Your browser does not support the video tag.
+    </video>
+
+
 </div>
 </div> 
 </div>
