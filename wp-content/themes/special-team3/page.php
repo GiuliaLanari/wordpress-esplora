2162237bc6e6fece723588516e19bae54f3c5725
chiaramarti <martinellichiara94@gmail.com>
2024-05-07 14:26:35 +0200

    page header + nav /no responsive

--- wp-content/themes/special-team3/page.php
+++ wp-content/themes/special-team3/page.php
@@ -1,35 +1,43 @@
 <?php
 
-get_header(); 
+use function FakerPress\get;
 
+get_header();
 ?>
-
-<div class="container clearfix">
-    <div class="header clearfix">
-        <a href="http://localhost/wordpress-esplora/home/" class="header__logo"> <img  src="<?php echo site_url("/wp-content/themes/special-team3/assets/img/epic-road.png") ?>" alt="logo" class="position-absolute mt-2 ms-2 " width="15%"/></a>
+<div class="position-relative">
 
     <?php
-         wp_nav_menu(array(
-             "theme_location" => "header",
-             "depth"=>"2",
-             "container"=>"false",
-             "container_class"=> "yo dude what col-auto",
-             "menu_class"=> "header__menu",
-         ))
+    include "nav.php";
     ?>
+    <!-- <div class="custom-background">
+
+// add_theme_support( 'custom-background' );
+</div> -->
+
+    <div class="container-fluid position-absolute  overflow-hidden p-0 ">
+        <!-- Logo -->
+        <?php
+        // Verifica se la pagina ha un'immagine in evidenza
+        if (has_post_thumbnail()) {
+            // Recupera l'ID dell'immagine in evidenza
+            $thumbnail_id = get_post_thumbnail_id();
+
+            // Recupera l'URL dell'immagine in evidenza di dimensioni "thumbnail"
+            $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'thumbnail')[0];
+
+            // Visualizza l'immagine in evidenza
+            echo '<img src="' . esc_url($thumbnail_url) . '" alt="' . get_the_title() . '">';
+        } else {
+            // Se la pagina non ha un'immagine in evidenza, mostra un'immagine predefinita o un messaggio alternativo
+            echo 'Nessuna immagine in evidenza disponibile.';
+        }
+        ?>
 
     </div>
 
-</div>
 
+    <?php
+    the_content();
 
-
-
-
-
-
-
-
-<?php
-
-get_footer();
+    include "footerStyle.php";
+    get_footer();
